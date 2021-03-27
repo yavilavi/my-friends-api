1edc94feb6e7d233d432c532f70ab12aa1b3f1f0
Yilmer Avila <yilmer0001@hotmail.com>
2021-03-27 11:00:45 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http')
+
+const server = http.createServer((request, response)=>{
+
+    console.log('Ruta: ', request.url);
+
+    // switch (request.url) {
+    //
+    //     case '/':
+    //         response.wr
+    //
+    // }
+
+
+});
+
+server.listen(4000,()=>{
+    console.log('Servidor operativo en puerto 4000 y dirección localhost');
+});
